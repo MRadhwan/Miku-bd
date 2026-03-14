752512bebb2bf9a6c0eae8684c7f3b7569c9d07f
MRadhwan <155960595+MRadhwan@users.noreply.github.com>
2026-03-15 02:43:54 +0400

    Update service worker to version 7 with new caching

--- sw.js
+++ sw.js
@@ -1,47 +1,48 @@
-const cacheName = 'miku-bd-v6'; 
-const assets = [
-  './',
-  './index.html',
-  './site.webmanifest',
-  './favicon-16x16.png',
-  './favicon-32x32.png',
-  './apple-touch-icon.png',
-  'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/Golden%20Brown%20-%20The%20Stranglers.mp3',
-  'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/Miku_Young.jpg',
-  'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/Miku_Present.jpg'
+const CACHE_NAME = 'miku-birthday-v7';
+const ASSETS = [
+    './',
+    './index.html',
+    './apple-touch-icon.png',
+    './favicon-32x32.png',
+    './favicon-16x16.png',
+    './site.webmanifest',
+    'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/Golden%20Brown%20-%20The%20Stranglers.mp3',
+    'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/Miku_Young.jpg',
+    'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/Miku_Present.jpg',
+    'https://raw.githubusercontent.com/MRadhwan/Miku-bd/main/android-chrome-512x512.png'
 ];
 
-// 1. Install Event - Force the new Service Worker to activate immediately
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Caching assets for v5');
-      return cache.addAll(assets);
-    })
-  );
-  // This skips the "waiting" phase and activates the new SW as soon as it's finished installing
-  self.skipWaiting();
+// Install Event - Caching the new v7 assets
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Forces the waiting service worker to become active immediately
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
-// 2. Activate Event - Clean up old caches and take control of the page
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== cacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-  // This allows the Service Worker to start controlling the page immediately without a manual refresh
-  return self.clients.claim();
+// Activate Event - Cleaning up ALL old caches (v1-v6)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing Old Cache', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Takes control of the page immediately
+    );
 });
 
-// 3. Fetch Event - Serve assets from cache
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+// Fetch Event - Serving assets from cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
